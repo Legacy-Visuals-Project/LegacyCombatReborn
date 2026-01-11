19cdd810f267e409e03b6fd53f7acc60c5dd7323
lowercasebtw <ignzoudy@gmail.com>
2026-01-10 23:30:42 -0500

    Refactor Handler

--- src/main/kotlin/org/visuals/legacy/handler/AnimatiumHandler.kt
+++ src/main/kotlin/org/visuals/legacy/handler/AnimatiumHandler.kt
@@ -1,11 +1,13 @@
 package org.visuals.legacy.handler
 
+import com.github.retrooper.packetevents.event.PacketListener
+import com.github.retrooper.packetevents.event.PacketReceiveEvent
 import org.visuals.legacy.LegacyCombatReborn
 import org.visuals.legacy.util.AnimatiumData
 import org.visuals.legacy.util.ServerFeature
 import java.util.*
 
-class AnimatiumHandler : Handler() {
+class AnimatiumHandler : Handler, PacketListener {
     private val players: Map<UUID, AnimatiumData> = mapOf()
 
     override fun init(plugin: LegacyCombatReborn) {
@@ -21,5 +23,8 @@ class AnimatiumHandler : Handler() {
     fun applyFeatures(uuid: UUID, features: Set<ServerFeature>) {
         if (!players.contains(uuid)) return // Doesn't have animatium
         // TODO;
+    }
+
+    override fun onPacketReceive(event: PacketReceiveEvent?) {
     }
 }
--- src/main/kotlin/org/visuals/legacy/handler/Handler.kt
+++ src/main/kotlin/org/visuals/legacy/handler/Handler.kt
@@ -1,10 +1,9 @@
 package org.visuals.legacy.handler
 
-import org.bukkit.event.Listener
 import org.visuals.legacy.LegacyCombatReborn
 
-abstract class Handler : Listener {
-    abstract fun init(plugin: LegacyCombatReborn)
+interface Handler {
+    fun init(plugin: LegacyCombatReborn)
 
-    abstract fun destroy(plugin: LegacyCombatReborn)
+    fun destroy(plugin: LegacyCombatReborn)
 }
